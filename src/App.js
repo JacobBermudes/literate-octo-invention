5d96ec24642aeb24a4ed098e65e316e95b0595d9
AntonyVa <132147071+AntonyVa@users.noreply.github.com>
2023-06-26 13:51:44 +0000

    npm

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Auth from "./components/authform"
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <AuthForm />
+
+    )
+  }
+}
+export default App
